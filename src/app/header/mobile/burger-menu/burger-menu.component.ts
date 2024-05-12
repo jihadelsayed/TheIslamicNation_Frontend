b99de40c74b61c452d6c60c78d22e1d569993c7c
Jihad Sayed <david.pales1@gmail.com>
2024-05-12 16:12:19 +0200

    header mobile

---
+++ src/app/header/mobile/burger-menu/burger-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-burger-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './burger-menu.component.html',
+  styleUrl: './burger-menu.component.scss'
+})
+export class BurgerMenuComponent {
+  @Input() init: boolean | undefined;
+  @Output() opened = new EventEmitter<any>();
+
+  active = false;
+
+  ngOnInit() {
+    this.active = this.init || false;
+  }
+
+  onBurgerClicked() {
+    this.active = !this.active;
+    this.opened.emit();
+  }
+}
--- src/app/header/mobile/mobile.component.ts
+++ src/app/header/mobile/mobile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { BurgerMenuComponent } from "./burger-menu/burger-menu.component";
 
 @Component({
-  selector: 'app-mobile',
-  standalone: true,
-  imports: [],
-  templateUrl: './mobile.component.html',
-  styleUrl: './mobile.component.scss'
+    selector: 'app-mobile',
+    standalone: true,
+    templateUrl: './mobile.component.html',
+    styleUrl: './mobile.component.scss',
+    imports: [BurgerMenuComponent]
 })
 export class MobileComponent {
 
