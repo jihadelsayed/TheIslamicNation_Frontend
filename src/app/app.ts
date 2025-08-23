4e42d3c76d9e526a4b1633e3d9ff0ab0d9ba031b
Jihad Sayed <david.pales1@gmail.com>
2025-08-23 09:41:04 -0500

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [CommonModule, RouterOutlet,HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
-export class AppComponent {
-  title = 'theislamicnation';
+export class App {
+  protected title = 'theislamicnation';
 }
